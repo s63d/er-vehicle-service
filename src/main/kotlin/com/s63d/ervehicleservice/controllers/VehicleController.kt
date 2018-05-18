24f0875387553e5acd74c7e9439126b1e5a7ea24
Marvin <marvin.zwolsman@gmail.com>
2018-05-18 17:31:07 +0200

    [WIP] decode jwt token

--- src/main/kotlin/com/s63d/ervehicleservice/controllers/VehicleController.kt
+++ src/main/kotlin/com/s63d/ervehicleservice/controllers/VehicleController.kt
@@ -1,5 +1,6 @@
 package com.s63d.ervehicleservice.controllers
 
+import io.jsonwebtoken.Jwts
 import org.springframework.http.HttpHeaders
 import org.springframework.web.bind.annotation.*
 
@@ -10,7 +11,14 @@ class VehicleController {
     @PostMapping
     fun createVehicle(@RequestParam license: String, @RequestParam type: String, @RequestParam brand: String,
                       @RequestParam weight: Int, @RequestHeader(HttpHeaders.AUTHORIZATION) authHeader: String) {
+        val uid = parseUid(authHeader)
+        println(uid)
+    }
 
-        println(authHeader)
+
+
+    private fun parseUid(rawHeader: String) : Long {
+        val header = rawHeader.replace("Bearer ", "")
+        return Jwts.parser().setSigningKey("c2VjcmV0").parseClaimsJws(rawHeader).body["userId"] as Long
     }
 }
