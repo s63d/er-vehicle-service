e0c00b4932118e4fcb361298a7fc9acc7c0b4bf2
Marvin <marvin.zwolsman@gmail.com>
2018-05-18 16:34:23 +0200

    Vehicle domain

--- src/main/kotlin/com/s63d/ervehicleservice/domain/Vehicle.kt
+++ src/main/kotlin/com/s63d/ervehicleservice/domain/Vehicle.kt
@@ -1,3 +1,7 @@
 package com.s63d.ervehicleservice.domain
 
-data class Vehicle(val id: Long)
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
+data class Vehicle(val type: String, val brand: String, val color: String, val weight: Int, val rate: Char, @Id val id: String = "")
