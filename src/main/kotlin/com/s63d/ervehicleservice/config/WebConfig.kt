5033f191fa35dac98c590e942e32273c73d7e56f
Marvin <marvin.zwolsman@gmail.com>
2018-05-20 21:40:54 +0200

    CORS fix

---
+++ src/main/kotlin/com/s63d/ervehicleservice/config/WebConfig.kt
@@ -0,0 +1,12 @@
+package com.s63d.ervehicleservice.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+    }
+}
