7b8b4828854f8a514ade65f67e51c00c3a0e647a
Marvin <marvin.zwolsman@gmail.com>
2018-05-20 13:32:58 +0200

    Set table name to account

--- src/main/kotlin/com/s63d/ervehicleservice/domain/SimpleAccount.kt
+++ src/main/kotlin/com/s63d/ervehicleservice/domain/SimpleAccount.kt
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.OneToMany
+import javax.persistence.Table
 
 @Entity
+@Table(name = "account")
 data class SimpleAccount (@Id val id: Long = 0, @OneToMany(mappedBy = "account") @JsonIgnore val ownerships:List<Ownership> = emptyList())
