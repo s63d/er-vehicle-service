f5672b1dbb2ffc8a9667ce11b7bc324df6867b03
Marvin <marvin.zwolsman@gmail.com>
2018-05-21 12:25:42 +0200

    Allow all methods not only GET, POST, HEAD

--- src/main/kotlin/com/s63d/ervehicleservice/config/WebConfig.kt
+++ src/main/kotlin/com/s63d/ervehicleservice/config/WebConfig.kt
@@ -8,5 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowedOrigins("*")
     }
 }
