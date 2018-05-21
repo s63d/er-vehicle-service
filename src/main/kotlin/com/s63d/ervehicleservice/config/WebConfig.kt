427934c84a9531fb6bdb277fead7a66b7364da56
Marvin <marvin.zwolsman@gmail.com>
2018-05-21 12:48:03 +0200

    Specify all the methods instead of wildcard

--- src/main/kotlin/com/s63d/ervehicleservice/config/WebConfig.kt
+++ src/main/kotlin/com/s63d/ervehicleservice/config/WebConfig.kt
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-                .allowedMethods("*")
+                .allowedMethods("GET", "PUT", "POST", "OPTIONS", "HEAD")
                 .allowedHeaders("*")
                 .allowedOrigins("*")
     }
