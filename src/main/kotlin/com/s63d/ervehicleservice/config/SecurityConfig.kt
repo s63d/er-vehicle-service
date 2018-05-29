b69b226435f8eada1e41e0cc128cda7741eb0c36
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-05-29 16:00:03 +0200

    Added spring security for token authorization

---
+++ src/main/kotlin/com/s63d/ervehicleservice/config/SecurityConfig.kt
@@ -0,0 +1,45 @@
+package com.s63d.ervehicleservice.config
+
+import com.s63d.ervehicleservice.security.JwtAuthorizationFilter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterAfter(JwtAuthorizationFilter(authenticationManager()), BasicAuthenticationFilter::class.java)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().headers().disable()
+    }
+
+    @Bean
+    fun corsConfigurationSource() : CorsConfigurationSource {
+        val cors = CorsConfiguration().apply {
+            applyPermitDefaultValues()
+            addExposedHeader(HttpHeaders.AUTHORIZATION)
+        }
+        return UrlBasedCorsConfigurationSource().apply { registerCorsConfiguration("/**", cors) }
+    }
+}
--- src/main/kotlin/com/s63d/ervehicleservice/controllers/VehicleController.kt
+++ src/main/kotlin/com/s63d/ervehicleservice/controllers/VehicleController.kt
@@ -1,36 +1,34 @@
 package com.s63d.ervehicleservice.controllers
 
-import com.s63d.ervehicleservice.services.JwtService
+import com.s63d.ervehicleservice.domain.Vehicle
 import com.s63d.ervehicleservice.services.OwnershipService
 import com.s63d.ervehicleservice.services.VehicleService
-import org.springframework.http.HttpHeaders
 import org.springframework.web.bind.annotation.*
+import java.security.Principal
 
-/*
-    TODO use spring-security for JWT parsing. Doing is manual is not ideal.
- */
 @RestController
 @RequestMapping("/api/vehicles")
-class VehicleController(private val vehicleService: VehicleService, private val ownershipService: OwnershipService, private val jwtService: JwtService) {
+class VehicleController(private val vehicleService: VehicleService, private val ownershipService: OwnershipService) {
 
     @GetMapping
-    fun allVehicles(@RequestParam user:Long?, @RequestHeader(HttpHeaders.AUTHORIZATION) authHeader: String) = vehicleService.vehicleRepository.findAll() // vehicleService.getForAccount(user ?: jwtService.getAccountId(authHeader))
+    fun allVehicles(): Iterable<Vehicle> = vehicleService.vehicleRepository.findAll()
 
     @PostMapping
     fun createVehicle(@RequestParam license: String, @RequestParam type: String, @RequestParam brand: String,
-                      @RequestParam weight: Int, @RequestParam color: String, @RequestHeader(HttpHeaders.AUTHORIZATION) authHeader: String) = vehicleService.createNew(license, type, brand, color, weight, jwtService.getAccountId(authHeader))
+                      @RequestParam weight: Int, @RequestParam color: String, principal: Principal
+    ) = vehicleService.createNew(license, type, brand, color, weight, principal.name.toLong())
 
 
     @GetMapping("current")
-    fun currentVehicles(@RequestParam user:Long?, @RequestHeader(HttpHeaders.AUTHORIZATION) authHeader: String) = ownershipService.currentVehicles(user ?: jwtService.getAccountId(authHeader))
+    fun currentVehicles(@RequestParam user:Long?, principal: Principal) = ownershipService.currentVehicles(user ?: principal.name.toLong())
 
     @GetMapping("history")
-    fun pastVehicles(@RequestParam user:Long?, @RequestHeader(HttpHeaders.AUTHORIZATION) authHeader: String) = ownershipService.pastVehicles(user ?: jwtService.getAccountId(authHeader))
+    fun pastVehicles(@RequestParam user:Long?, principal: Principal) = ownershipService.pastVehicles(user ?: principal.name.toLong())
 
     @GetMapping("{license}")
     fun byLicense(@PathVariable license: String) = vehicleService.getByLicense(license)
 
     @PostMapping("{license}/suspend")
-    fun suspendVehicle(@PathVariable license: String, @RequestHeader(HttpHeaders.AUTHORIZATION) authHeader: String) = vehicleService.suspend(license, jwtService.getAccountId(authHeader))
+    fun suspendVehicle(@PathVariable license: String, principal: Principal) = vehicleService.suspend(license, principal.name.toLong())
 
 }
