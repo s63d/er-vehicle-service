bda2b6db2044b4906725d6e2b371b631b0dd2d4b
Gregory <gregory_lammers@hotmail.com>
2018-06-04 21:47:58 +0200

    add ownerid

--- src/main/kotlin/com/s63d/ervehicleservice/domain/rest/GovernmentVehicle.kt
+++ src/main/kotlin/com/s63d/ervehicleservice/domain/rest/GovernmentVehicle.kt
@@ -3,7 +3,7 @@ package com.s63d.ervehicleservice.domain.rest
 import com.s63d.ervehicleservice.domain.db.Ownership
 import java.util.*
 
-data class GovernmentVehicle(val id: String, val type: String, val brand: String, val color: String, val weight: Int,
+data class GovernmentVehicle(val id: String, val ownerId: Long, val type: String, val brand: String, val color: String, val weight: Int,
                              val registrationDate: Date, val endDate: Date?, val carTrackerId: String? = null, val rate: Char, val stolen: Boolean = false) {
-    constructor(ownership: Ownership) : this(ownership.vehicle.id, ownership.vehicle.type, ownership.vehicle.brand, ownership.vehicle.color, ownership.vehicle.weight, ownership.startDate, ownership.endDate, ownership.vehicle.carTracker?.id, ownership.vehicle.rate)
+    constructor(ownership: Ownership) : this(ownership.vehicle.id, ownership.account.id, ownership.vehicle.type, ownership.vehicle.brand, ownership.vehicle.color, ownership.vehicle.weight, ownership.startDate, ownership.endDate, ownership.vehicle.carTracker?.id, ownership.vehicle.rate)
 }
