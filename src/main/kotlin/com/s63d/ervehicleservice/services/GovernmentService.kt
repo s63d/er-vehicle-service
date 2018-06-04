0d58a25c4f911408effea39ac8f2790401a70fc1
Marvin <marvin.zwolsman@gmail.com>
2018-06-04 19:01:50 +0200

    Government car service

--- src/main/kotlin/com/s63d/ervehicleservice/services/GovernmentService.kt
+++ src/main/kotlin/com/s63d/ervehicleservice/services/GovernmentService.kt
@@ -6,9 +6,23 @@ import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
 @Service
-class GovernmentService(private val vehicleRepository: VehicleRepository) {
+class GovernmentService(private val vehicleRepository: VehicleRepository, private val carTrackerService: CarTrackerService) {
 
     fun getCars(pageable: Pageable = Pageable.unpaged()) =  vehicleRepository.findAll(pageable).map {
         GovernmentVehicle(it.ownership)
     }
+
+    fun assignCartracker(id: String): GovernmentVehicle {
+        val vehicle = vehicleRepository.findById(id).orElseThrow { Exception("Car $id not found") }
+        vehicle.carTracker = carTrackerService.createNew()
+
+        return GovernmentVehicle(
+                vehicleRepository.save(vehicle).ownership
+        )
+    }
+
+    fun getCar(id: String) = GovernmentVehicle(
+            vehicleRepository.findById(id).orElseThrow { Exception("Car $id not found") }
+                    .ownership
+    )
 }
